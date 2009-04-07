bd9218929e54eb5f2c832b7d09878d4088e4be74
Albert Callarisa <shark234@gmail.com>
2009-04-07 11:37:14 +0200

    Documented required-fields code

--- lib/ubiquo/required_fields.rb
+++ lib/ubiquo/required_fields.rb
@@ -1,4 +1,35 @@
+
 module Ubiquo
+  
+  #Usage:
+  #Add 'required_fields' to your ActiveRecord model:
+  # class YourModel < ActiveRecord::Base
+  #   required_fields :name
+  # end
+  #
+  #If you use 'validates_pressence_of' it will automaticaly detected as required field.
+  #
+  #Once a field is marked as required, it will show an asterisk(*) in field label inside the form
+  #
+  # <% form_for ... do |form|>
+  #   <%= form.label :name, "Name" %>
+  # <% end %>
+  #
+  #this label will be 
+  # <label for="yourmodel_name">Name *</label>
+  #
+  #
+  #
+  #This extension also provides an optional value for label_tag helper
+  #named 'append_asterisk' that only appends an asterisk after the label content.
+  #
+  #
+  #This asterisk can be manually disabled from view, just adding :append_asterisk => false to the helper options:
+  # <%= form.label :name, "Name", :append_asterisk => false %>
+  #
+  #
+  
+  
   module RequiredFields
     autoload :ActiveRecord, "ubiquo/required_fields/active_record.rb"
     autoload :Validations, "ubiquo/required_fields/validations.rb"
