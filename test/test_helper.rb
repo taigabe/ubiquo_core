a0c2650c6703a5f1c61a6037225212dc2145f839
Albert Callarisa <shark234@gmail.com>
2009-05-08 12:04:24 +0200

    Removed notices from tests and fixed error when no ubiquo_i18n is present

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,6 @@
 require File.dirname(__FILE__) + "/../../../../test/test_helper.rb"
+
+case conn = ActiveRecord::Base.connection
+when ActiveRecord::ConnectionAdapters::AbstractAdapter
+  conn.client_min_messages = "ERROR"
+end
