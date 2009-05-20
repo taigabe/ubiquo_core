fbef21c912e6408f22b1d9caa325e755fd5d335d
Ramon Salvadó <rsalvado@gnuine.com>
2009-05-20 11:40:15 +0200

    Fixed, ubiquo will only work with the pg gem.
    It won't work with the old postgres gem.
    [#102 state:resolved]

--- lib/ubiquo/adapters/postgres.rb
+++ lib/ubiquo/adapters/postgres.rb
@@ -15,11 +15,11 @@ module Ubiquo
         end
         
         def list_sequences(starts_with)
-          self.execute("SELECT c.relname AS sequencename FROM pg_class c WHERE (c.relkind = 'S' and c.relname ILIKE E'#{starts_with}%');").rows.flatten
+          self.execute("SELECT c.relname AS sequencename FROM pg_class c WHERE (c.relkind = 'S' and c.relname ILIKE E'#{starts_with}%');").entries.map { |result| result['sequencename'] }
         end
         
         def next_val_sequence(name)
-          self.execute("SELECT nextval('%s');" % name).rows.first.first.to_i
+          self.execute("SELECT nextval('%s');" % name).entries.first['nextval'].to_i
         end
       end
     end
