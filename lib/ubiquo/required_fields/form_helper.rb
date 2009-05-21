8c13d85708cc1c79359a70ad81b830fdf3c6286a
Albert Callarisa <shark234@gmail.com>
2009-05-21 23:35:35 +0200

    Fixed NameError in labels. It is raised when used required fields in a non model-based forms

--- lib/ubiquo/required_fields/form_helper.rb
+++ lib/ubiquo/required_fields/form_helper.rb
@@ -10,11 +10,14 @@ module Ubiquo
       module InstanceMethods
         # Adds "append_asterisk" option to 'options' parameter if the field is maked as required.
         def label_with_asterisk(object_name, method, text = nil, options = {})
-          object_class = options[:object] ? options[:object].class : object_name.to_s.classify.constantize
-          if object_class && 
-              object_class.respond_to?(:required_fields) && 
-              object_class.required_fields.include?(method)
-            options.reverse_merge!("append_asterisk" => true) 
+          begin
+            object_class = options[:object] ? options[:object].class : object_name.to_s.classify.constantize
+            if object_class && 
+                object_class.respond_to?(:required_fields) && 
+                object_class.required_fields.include?(method)
+              options.reverse_merge!("append_asterisk" => true) 
+            end
+          rescue NameError => e
           end
           label_without_asterisk(object_name, method, text, options)
         end
