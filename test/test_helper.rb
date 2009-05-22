ce8b1458b3d14c3c9990f8ddfb68129c191f0e0b
Toni Reina <areina0@gmail.com>
2009-05-22 16:44:06 +0200

    Updated client_min_messages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 require File.dirname(__FILE__) + "/../../../../test/test_helper.rb"
 
-case conn = ActiveRecord::Base.connection
-when ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+if ActiveRecord::Base.connection.class.to_s == "ActiveRecord::ConnectionAdapters::PostgreSQLAdapter"
   conn.client_min_messages = "ERROR"
 end
