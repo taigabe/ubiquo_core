16a3ee486e53518b103859d75e88147b106b64f0
Albert Callarisa <shark234@gmail.com>
2009-05-26 11:18:56 +0200

    Fixed test_helper bug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + "/../../../../test/test_helper.rb"
 
 if ActiveRecord::Base.connection.class.to_s == "ActiveRecord::ConnectionAdapters::PostgreSQLAdapter"
-  conn.client_min_messages = "ERROR"
+  ActiveRecord::Base.connection.client_min_messages = "ERROR"
 end
