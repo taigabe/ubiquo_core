32fc39b9aa9d3fa8ab740e960fad5230c6ad6235
Bernat Foj Capell <bfoj@gnuine.com>
2009-06-10 13:33:42 +0200

    Included $ symbol to mark the final of table names in sequence names [#128 state:resolved]

--- lib/ubiquo/adapters/table_definition.rb
+++ lib/ubiquo/adapters/table_definition.rb
@@ -5,7 +5,7 @@ module Ubiquo
       # Creates an integer and an associated sequence field
       def sequence(table_name, field_name)
         integer field_name, :null => false 
-        ActiveRecord::Base.connection.create_sequence("%s_%s" % [table_name, field_name])
+        ActiveRecord::Base.connection.create_sequence("%s_$_%s" % [table_name, field_name])
       end
     end
   end
--- test/unit/adapters_test.rb
+++ test/unit/adapters_test.rb
@@ -16,7 +16,7 @@ class Ubiquo::AdaptersTest < ActiveSupport::TestCase
   end
   
   def test_create_sequence
-    ActiveRecord::Base.connection.expects(:create_sequence).with("test_content_id").once
+    ActiveRecord::Base.connection.expects(:create_sequence).with("test_$_content_id").once
     
     definition = nil
     ActiveRecord::Base.connection.create_table(:test){|table|
@@ -30,17 +30,17 @@ class Ubiquo::AdaptersTest < ActiveSupport::TestCase
     ActiveRecord::Base.connection.create_table(:test){|table|
       table.sequence :test, :content_id
     }
-    assert ActiveRecord::Base.connection.list_sequences("test_").include?('test_content_id')
+    assert ActiveRecord::Base.connection.list_sequences("test_").include?('test_$_content_id')
   end
   
   def test_drop_created_sequences
     ActiveRecord::Base.connection.create_table(:test){|table|
       table.sequence :test, :content_id
     }
-    assert ActiveRecord::Base.connection.list_sequences("test_").include?('test_content_id')
+    assert ActiveRecord::Base.connection.list_sequences("test_").include?('test_$_content_id')
     
     ActiveRecord::Base.connection.drop_table(:test)
-    assert !ActiveRecord::Base.connection.list_sequences("test_").include?('test_content_id')
+    assert !ActiveRecord::Base.connection.list_sequences("test_").include?('test_$_content_id')
     
   end
   
