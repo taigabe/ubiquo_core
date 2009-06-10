984f6eb0d7e5c0424b6b3b6f222b6ecbc2b9ff60
Bernat Foj Capell <bfoj@gnuine.com>
2009-06-10 17:09:55 +0200

    Removed bad line

--- test/unit/ubiquo/extensions/active_record_test.rb
+++ test/unit/ubiquo/extensions/active_record_test.rb
@@ -3,7 +3,6 @@ require File.dirname(__FILE__) + "/../../../test_helper.rb"
 class Ubiquo::Extensions::ActiveRecordTest < ActiveSupport::TestCase
 
   def test_should_have_a_paginate
-    ActiveRecord::Base.paginate
     assert ActiveRecord::Base.methods.include?('paginate')
   end
   
