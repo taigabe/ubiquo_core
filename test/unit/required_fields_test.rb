543561bfddda8b63e1d614446cc940b079657e1a
Bernat Foj Capell <bfoj@gnuine.com>
2009-06-10 13:24:25 +0200

    Renamed TestModel class [#127 state:resolved]

--- test/unit/required_fields_test.rb
+++ test/unit/required_fields_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + "/../test_helper.rb"
 class Ubiquo::RequiredFieldsTest < ActiveSupport::TestCase
   include Ubiquo::Extensions::ConfigCaller
   def test_add_expected_required_fields
-    assert_equal [:name, :title], TestModel.required_fields
+    assert_equal [:name, :title], TestModelRequired.required_fields
   end
   
   def test_add_expected_required_fields_in_iherited_models
@@ -11,12 +11,12 @@ class Ubiquo::RequiredFieldsTest < ActiveSupport::TestCase
   end
 end
 
-class TestModel < ActiveRecord::Base
+class TestModelRequired < ActiveRecord::Base
   validates_presence_of :name, :if => lambda{|m| m.id == 1}
   required_fields :title
 end
 
-class InheritedModel < TestModel
+class InheritedModel < TestModelRequired
   validates_presence_of :surname, :if => lambda{|m| m.id == 1}
   required_fields :subtitle
 end
