5eeb0f0855ef2860d21a1e05a99a84014273af04
Bernat Foj Capell <bfoj@gnuine.com>
2010-01-27 19:55:59 +0100

    Added helpers for show/preview page [#58 state:updated]

--- lib/ubiquo/helpers.rb
+++ lib/ubiquo/helpers.rb
@@ -6,4 +6,5 @@ module Ubiquo
 end
 
 Ubiquo::Extensions::UbiquoAreaController.append_helper(Ubiquo::Helpers::CoreUbiquoHelpers)
+Ubiquo::Extensions::UbiquoAreaController.append_helper(Ubiquo::Helpers::ShowHelpers)
 ActionController::Base.helper(Ubiquo::Helpers::CorePublicHelpers)
