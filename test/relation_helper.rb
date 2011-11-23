627e39babea3ac328e4dea5f1cc1c367cd3781c6
Bernat Foj Capell <bfoj@gnuine.com>
2011-11-23 14:44:47 +0100

    Added :distinct option in Rails finders [#627 state:resolved]

--- test/relation_helper.rb
+++ test/relation_helper.rb
@@ -1,19 +1,14 @@
 #RELATION SELECTOR ONLY TEST
-require 'mocha'
+
 def create_relation_test_model_backend
-  # Creates a test table for AR things work properly
-  %w{test_only_models middle_tables test_only_model_twos}.each do |table|
-    if ActiveRecord::Base.connection.tables.include?(table)
-      ActiveRecord::Base.connection.drop_table table
-    end
-  end
+
   ActiveRecord::Base.connection.create_table :test_only_models do |t|
     t.integer :test_only_model_two_id #for belongs_to with the same class name
     t.integer :arbitrary_name_id #for belongs to with different class
     #name
     t.string :name
     t.string :arbitrary_name
-  end
+  end unless ActiveRecord::Base.connection.tables.include?('test_only_models')
 
   ActiveRecord::Base.connection.create_table :test_only_model_twos do |t|
     t.integer :test_only_model_id #for belongs_to with the same class name
@@ -21,17 +16,18 @@ def create_relation_test_model_backend
     #name
     t.string :title
     t.string :arbitrary_name
-  end
+  end unless ActiveRecord::Base.connection.tables.include?('test_only_model_twos')
 
   ActiveRecord::Base.connection.create_table :middle_tables do |t|
     t.integer :test_only_model_id
     t.integer :test_only_model_two_id
-  end
+  end unless ActiveRecord::Base.connection.tables.include?('middle_tables')
 
   %w{TestOnlyModel MiddleTable TestOnlyModelTwo}.each do |klass|
     Object.const_set(klass, Class.new(ActiveRecord::Base)) unless Object.const_defined? klass
   end
-  
+
+  set_relations
 end
 
 def set_relations options = {}
@@ -48,9 +44,11 @@ def set_relations options = {}
     belongs_to :related, :foreign_key => :arbitrary_name_id, :class_name => TestOnlyModel.name
     belongs_to :test_only_model
   end
-  
+
   MiddleTable.class_eval do
     belongs_to :test_only_model
     belongs_to :test_only_model_two
   end
 end
+
+create_relation_test_model_backend
