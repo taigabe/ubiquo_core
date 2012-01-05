31f569722966b4fd185b058ff2f478e411c6f9ff
Bernat Foj Capell <bfoj@gnuine.com>
2012-01-05 11:18:54 +0100

    Specify utf8 as the file encoding to avoid ruby 1.9 error [#674]

--- lib/ubiquo/extensions/string.rb
+++ lib/ubiquo/extensions/string.rb
@@ -1,27 +1,28 @@
+# encoding: utf-8
 module Ubiquo
   module Extensions
     module String
 
-      # Remove all non-alphanumeric characters from the string, trying to do 
-      # a wide conversion (though non complete) of non-english characters 
-      def urilize    
+      # Remove all non-alphanumeric characters from the string, trying to do
+      # a wide conversion (though non complete) of non-english characters
+      def urilize
         pattern_replacements = [
                                 [/[àáâäÀÁÂÄ]/, "a"],
-                                [/[èéêëÈÉÊË]/, "e"],      
+                                [/[èéêëÈÉÊË]/, "e"],
                                 [/[ìíîïÌÍÎÏ]/, "i"],
                                 [/[òóôöÒÓÔÖ]/, "o"],
                                 [/[ùúûüÙÚÛÜ]/, "u"],
                                 [/[\/(){}<>]/, "_"],
                                 [/[ñÑ]/, "n"],
                                 [/[çÇ]/, "c"],
-                                [/[^\w]/, ''], # finally, discard all non-alphanumeric characters 
+                                [/[^\w]/, ''], # finally, discard all non-alphanumeric characters
                                ]
-        start_string = self.downcase.strip.gsub(" ", "")    
-        pattern_replacements.inject(start_string) do |s, (pattern, replacement)| 
+        start_string = self.downcase.strip.gsub(" ", "")
+        pattern_replacements.inject(start_string) do |s, (pattern, replacement)|
           s.gsub(pattern, replacement)
-        end      
+        end
       end
-      
+
       # allowed options:
       # :max_chars - the maximum length of the result. Includes the omissions
       # :omission - the string to show when the text is truncated. Default "..."
@@ -32,18 +33,18 @@ module Ubiquo
       # Look the StringTest file to see examples of use.
       def truncate_words(options = {})
         stripped = ActionController::Base.helpers.strip_tags(self)
-        
+
         max_length = options[:max_chars] || 100
         omission = options[:omission] || "..."
         center = options[:center]
         highlight = [options[:highlight]].flatten.compact
         highlight_class = options[:highlight_class] || "highlight"
-        
+
         if max_length < stripped.length
           if center
             r_limit = stripped.index(center) + center.length + ((max_length - center.length) / 2) - 1 - omission.length
             l_limit = stripped.index(center) - ((max_length - center.length) / 2) + omission.length
-            
+
             if l_limit < 0
               r_limit -= l_limit
               r_limit += omission.length
@@ -61,7 +62,7 @@ module Ubiquo
             if r_limit < stripped.length && stripped[r_limit + 1,1] != " "
               result = result[0..(result.rindex(" ")-1)]
             end
-            
+
             result = omission + result + omission
           else
             limit = max_length - 1 - omission.length
@@ -78,13 +79,13 @@ module Ubiquo
         else
           result = stripped
         end
-        
+
         highlight.each do |h|
           result = result.gsub(h, "<span class=\"#{highlight_class}\">#{h}</span>")
         end
         result
-        
-        
+
+
       end
     end
   end
