fa8fdaba7d0d444a62cf01aa3fb627eb26210c39
Bernat Foj Capell <bfoj@gnuine.com>
2012-01-10 10:56:26 +0100

    drop_table no longer accepts an options hash (rails 3.2) [#674]

--- lib/ubiquo/adapters/schema_statements.rb
+++ lib/ubiquo/adapters/schema_statements.rb
@@ -1,15 +1,15 @@
 module Ubiquo
   module Adapters
     module SchemaStatements
-      
+
       def self.included(klass)
         klass.send(:alias_method_chain, :drop_table, :sequences)
       end
-      
+
       # Performs the usual drop_table action, but remove also the created sequences
       # that are related with this table
       def drop_table_with_sequences(table_name, options={})
-        drop_table_without_sequences(table_name, options)
+        drop_table_without_sequences(table_name)
         ActiveRecord::Base.connection.list_sequences(table_name.to_s + "_$").each do |sequence|
           unless sequence =~ /id_seq/ || options[:force]
             ActiveRecord::Base.connection.drop_sequence sequence
