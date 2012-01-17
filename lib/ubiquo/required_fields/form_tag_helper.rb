5551a18b6bd0d1085b0c040a93cb61d26d24618e
Bernat Foj Capell <bfoj@gnuine.com>
2012-01-17 16:27:11 +0100

    Refactoring to avoid a deprecation warning

--- lib/ubiquo/required_fields/form_tag_helper.rb
+++ lib/ubiquo/required_fields/form_tag_helper.rb
@@ -1,23 +1,19 @@
 module Ubiquo
   module RequiredFields
     module FormTagHelper
-      
+
       def self.included(klass)
-        klass.send :include, InstanceMethods
         klass.send :alias_method_chain, :label_tag, :asterisk
       end
-      
-      module InstanceMethods
-        
-        # appends an asterisk to the text if needed
-        def label_tag_with_asterisk(name, text = nil, options = {})
-          if !text.nil? && options["append_asterisk"] == true
-            span_class = Ubiquo::Settings.get(:required_field_class)
-            text += "<span class= #{span_class} > * </span>"
-          end
-          options.delete("append_asterisk")
-          label_tag_without_asterisk(name, text, options)
+
+      # appends an asterisk to the text if needed
+      def label_tag_with_asterisk(name, text = nil, options = {})
+        if !text.nil? && options["append_asterisk"] == true
+          span_class = Ubiquo::Settings.get(:required_field_class)
+          text += "<span class= #{span_class} > * </span>".html_safe
         end
+        options.delete("append_asterisk")
+        label_tag_without_asterisk(name, text, options)
       end
     end
   end
