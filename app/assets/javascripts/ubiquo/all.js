924aedf9fda751a411753c64c2388023716fb757
Toni Reina <areina0@gmail.com>
2012-01-20 14:41:56 +0100

    Adapt stylesheets, javascripts and images to new asset pipeline way [#674]

---
+++ app/assets/javascripts/ubiquo/all.js
@@ -0,0 +1,9 @@
+//= require prototype
+//= require prototype_ujs
+//= require effects
+//= require dragdrop
+//= require controls
+//= require ubiquo/lightwindow
+//= require ubiquo/lightwindow_ubiquo
+//= require ubiquo/relation_selector
+//= require_tree .
