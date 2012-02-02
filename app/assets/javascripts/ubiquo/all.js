0222655d8e40b923fda04f58bf086fae2acc1600
Jon de Andres <jandres@gnuine.com>
2012-02-02 15:01:30 +0100

    Move/Rename/Add ubiquo_core assets [#674]

--- app/assets/javascripts/ubiquo/all.js
+++
@@ -1,9 +0,0 @@
-//= require prototype
-//= require prototype_ujs
-//= require effects
-//= require dragdrop
-//= require controls
-//= require ubiquo/lightwindow
-//= require ubiquo/lightwindow_ubiquo
-//= require ubiquo/relation_selector
-//= require_tree .
